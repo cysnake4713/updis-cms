49ed7b997fb3129b07edb8d64da277c981887ba2
cysnake4713 <cysnake4713@gmail.com>
2013-04-05 18:18:10 +0800

    UPDIS-61 blocked

---
+++ gunicorn.conf.py
@@ -0,0 +1,4 @@
+bind = "127.0.0.1:8001"
+logfile = "/home/cysnake4713/gunicorn.log"
+workers = 3
+timeout = 60
--- upcms/settings.py
+++ upcms/settings.py
@@ -59,7 +59,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, 'sitestatic')
+STATIC_ROOT = os.path.join(PROJECT_PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -198,7 +198,7 @@ LANGUAGES = [
     ('zh-CN', 'Chinese'),
 ]
 
-ERP_HOST = "10.100.100.14"
+ERP_HOST = "127.0.0.1"
 ERP_PORT = 8069
 ERP_LOGIN = 'cmsreader'
 ERP_PASSWORD = 'cmsreader'
