eb4d48e870cc80e28798d1cd6945ba28e0a51e7f
cysnake4713 <cysnake4713@gmail.com>
2013-04-28 14:34:15 +0800

    发布准备

--- upcms/settings_production.py
+++ upcms/settings_production.py
@@ -2,9 +2,9 @@ __author__ = 'Zhou Guangwen'
 
 import settings
 
-settings.ERP_HOST = "erp.updis.cn"
+settings.ERP_HOST = "10.100.100.14"
 settings.DEBUG = TEMPLATE_DEBUG = False
-settings.CAS_SERVER_URL = "http://sso.updis.cn/cas/"
+settings.CAS_SERVER_URL = "http://sso.updis.cn:81/cas/"
 
 CACHES = {
     'default': {
