cedfee0034a3f713a6250108c5892888c7683761
cysnake4713 <cysnake4713@gmail.com>
2013-05-14 17:03:02 +0800

    UPDIS-171 优化cms首页速度！

--- messages/task.py
+++ messages/task.py
@@ -7,4 +7,5 @@ CMS_HOME = 'http://127.0.0.1:8001'
 if __name__ == "__main__":
 
     TYPE = sys.argv[1]
-    urllib2.urlopen(CMS_HOME + '/message/reload/%s/' % TYPE)
+    connection = urllib2.urlopen(CMS_HOME + '/message/reload/%s/' % TYPE)
+    connection.close()
