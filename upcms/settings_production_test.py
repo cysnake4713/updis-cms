28de89f1a3e55a3971a8c5c7e6dbbe948305048e
cysnake4713 <cysnake4713@gmail.com>
2013-05-29 13:27:24 +0800

    UPDIS-177 分离了测试服务器的配置文件

---
+++ upcms/settings_production_test.py
@@ -0,0 +1,24 @@
+__author__ = 'Zhou Guangwen'
+
+import settings
+
+settings.ERP_HOST = "10.100.100.171"
+settings.DEBUG = TEMPLATE_DEBUG = True
+settings.CAS_SERVER_URL = "http://sso.updis.cn/cas/"
+settings.ERP_DOMAIN = ".updis.cn"
+settings.ERP_HOME = 'http://terp.updis.cn'
+
+settings.DB_HOST = "10.100.100.172"
+settings.DB_NAME = "develop"
+settings.DB_USER = "openerp_updis"
+settings.DB_PASSWORD = "openerpupdis2013"
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': 'localhost:11211',
+    }
+}
+
+from settings import *
+
