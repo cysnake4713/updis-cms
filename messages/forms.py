a7cb0fa34f874c86abd587767a276e95d92e8414
cysnake4713 <cysnake4713@gmail.com>
2013-07-26 17:59:18 +0800

    UPDIS-252 消息评论字数限制问题，取消限制吧好多评论发不出来

--- messages/forms.py
+++ messages/forms.py
@@ -6,7 +6,7 @@ from django import forms
 
 
 class CommentForm(forms.Form):
-    body = forms.CharField(max_length=200, widget=forms.Textarea)
+    body = forms.CharField(max_length=5000, widget=forms.Textarea)
     is_anonymous = forms.BooleanField(required=False, initial=False)
     attachment = forms.FileField(required=False)
 
