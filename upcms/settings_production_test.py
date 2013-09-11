55d1049e3180429da935abeab6ed990b0548d5f2
cysnake4713 <cysnake4713@gmail.com>
2013-09-11 16:45:41 +0800

    UPDIS-236 cms页面分块刷新，解决首页内容刷新严重滞后的问题

--- upcms/settings_production_test.py
+++ upcms/settings_production_test.py
@@ -3,7 +3,7 @@ __author__ = 'Zhou Guangwen'
 import settings
 
 settings.ERP_HOST = "10.100.100.171"
-settings.ERP_HOST = "localhost"
+# settings.ERP_HOST = "localhost"
 settings.DEBUG = TEMPLATE_DEBUG = True
 settings.CAS_SERVER_URL = "http://sso.updis.cn/cas/"
 settings.ERP_DOMAIN = ".updis.cn"
